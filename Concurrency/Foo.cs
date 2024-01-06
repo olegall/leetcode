5712e580ed0d265c24b08c0faebe604d0eacbf32
Oleg <aleynikov.oleg@gmail.com>
2024-01-06 13:36:42 +0300

    initial

---
+++ Concurrency/Foo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace Leetcode.Concurrency
+{
+    public class Foo
+    {
+        private static Semaphore s1;
+        private static Semaphore s2;
+
+        public Foo()
+        {
+            s1 = new Semaphore(initialCount: 0, maximumCount: 1);
+            s2 = new Semaphore(initialCount: 0, maximumCount: 1);
+        }
+
+        public void First(Action printFirst)
+        {
+            printFirst();
+            s1.Release();
+        }
+
+        public void Second(Action printSecond)
+        {
+            s1.WaitOne();
+            printSecond();
+            s2.Release(); // Если в цикле. System.Threading.SemaphoreFullException: "Adding the specified count to the semaphore would cause it to exceed its maximum count."
+        }
+
+        public void Third(Action printThird)
+        {
+            s2.WaitOne();
+            printThird();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using Leetcode.Concurrency;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++) // for асинхронный?
+            {
+                Threads();
+                Thread.Sleep(500);
+                Console.WriteLine();
+            }
+            //Console.WriteLine("Hello World!");
+        }
+
+        private static void Threads()
+        {
+            Action a1 = delegate() { Console.WriteLine("first"); };
+            Action a2 = delegate() { Console.WriteLine("second"); };
+            Action a3 = delegate() { Console.WriteLine("third"); };
+
+            var foo = new Foo();
+
+            new Thread(() => { foo.First(a1); }).Start();
+            new Thread(() => { foo.Second(a2); }).Start();
+            new Thread(() => { foo.Third(a3); }).Start();
+
+            //Task.Run(() => foo.First(a1));
+            //Task.Run(() => foo.Second(a2));
+            //Task.Run(() => foo.Third(a3));
+        }
+    }
+}
